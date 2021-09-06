585bfa54fcef15cca88a179cb7ba7d94d48a32ce
Tim Hagn <mail@timhagn.com>
2021-09-06 20:04:17 +0200

    add input changed-files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+const exec = require('@actions/exec');
 
 try {
-  const changedFiles = core.getInput("CHANGED_FILES");
+  const changedFiles = core.getInput("changed-files");
   core.setOutput("changed", JSON.stringify(changedFiles));
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
