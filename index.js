fd4e1241006f90491e2f0f381acf6385059b7b0c
Tim Hagn <mail@timhagn.com>
2021-09-06 20:06:00 +0200

    update dist

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const core = require("@actions/core");
-// const github = require("@actions/github");
+const github = require("@actions/github");
 const exec = require('@actions/exec');
 
 try {
   const changedFiles = core.getInput("changed-files");
   core.setOutput("changed", JSON.stringify(changedFiles));
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  core.debug(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // core.debug(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
