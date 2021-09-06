66d4f7f78e5da6fef74dfabb48c78ac16c94f80e
Tim Hagn <mail@timhagn.com>
2021-09-06 18:55:42 +0200

    replace console.log with core.debug

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const github = require("@actions/github");
 
 try {
   const changedFiles = core.getInput("CHANGED_FILES");
-  console.log(JSON.stringify(changedFiles));
+  core.debug(JSON.stringify(changedFiles));
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  core.debug(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
