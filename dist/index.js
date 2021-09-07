b034455d89595a9d935f5e5f0c0267ec21a0847b
swmd <frontdev8@gmail.com>
2021-09-07 16:00:16 -0400

    update dist

--- dist/index.js
+++ dist/index.js
@@ -5751,20 +5751,23 @@ const ERRORS = {
 const CATEGORIES = [
   'amm',
   'app',
+  'dapp',
   'defi',
   'dex',
   'exchange',
   'explorer',
+  'fund',
   'governance',
   'infra',
+  'investmentfund',
+  'metaplex',
+  'nft',
   'oracle',
+  'sdk',
   'spl',
   'stablecoin',
   'tools',
-  'nft',
   'wallet',
-  'fund',
-  'investmentfund'
 ];
 
 module.exports = {
