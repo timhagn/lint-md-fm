fffbe1fc0b5a9801b0b8560dae5be38ff97cc506
swmd <frontdev8@gmail.com>
2021-09-14 00:39:48 -0400

    add comments to duplication check

--- src/testDuplication.js
+++ src/testDuplication.js
@@ -5,6 +5,13 @@ const Fuse = require("fuse.js");
 
 const { STATUS, ERRORS } = require("./constants");
 
+/**
+ * Loop over the markdown directory and grabs all the existing slugs by parsing the files.
+ * 
+ * @param {[string]} extensions 
+ * @param {string} directory 
+ * @returns {{list: [{slug, filePath}]}}
+ */
 const getSlugList = (extensions, directory) => {
   let list = [];
   const allFiles = fs.readdirSync(directory);
@@ -25,17 +32,27 @@ const getSlugList = (extensions, directory) => {
   return list;
 };
 
+/**
+ * 
+ * @param slugList            Existing slugs list
+ * @param slug                slug of the new project
+ * @param filePath            Path of the new project markdown
+ * @param isFuzzySearch       Indicate if the fuzzy search is enabled
+ * @returns                   Array of duplicate projects
+ */
 const checkDuplication = (slugList, slug, filePath, isFuzzySearch) => {
   let result = [];
+
   if (isFuzzySearch) {
     const fuse = new Fuse(slugList, {
       includeScore: true,
       threshold: 0.2,
       keys: ["slug"],
     });
-    const duplications = fuse.search(slug);
+    const duplications = fuse.search(slug);     // Grab all matches using fuse.js
 
     duplications.forEach((old) => {
+      // Exclude the new project
       if (old.item.filePath !== filePath) {
         result.push(old.item.filePath);
       }
@@ -54,10 +71,10 @@ const checkDuplication = (slugList, slug, filePath, isFuzzySearch) => {
 /**
  * Loops over all files in a directory and checks project duplication.
  *
- * @param changedFiles
+ * @param changedFiles Valid changed markdown files from the previous step.
  * @param markdownExtensions
- * @param directory
- * @param isFuzzySearch
+ * @param directory  Markdown directory
+ * @param isFuzzySearch Parameter to indicate if fuzzy search is enabled
  * @returns {{errors: *[], status: string}}
  */
 const testDuplication = (
@@ -70,13 +87,14 @@ const testDuplication = (
     status: STATUS.VALID,
     errors: [],
   };
-  const slugList = getSlugList(markdownExtensions, directory);
+  const slugList = getSlugList(markdownExtensions, directory);          // Grab all slugs from the project directory
+  // Loop over all the valid changed markdown files
   changedFiles.forEach((filePath) => {
-    // check markdown files in the specified directory
     const markdownData = fs.readFileSync(filePath, "utf8");
     const parsed = matter(markdownData);
     if (parsed.data && parsed.data.slug) {
       const slug = parsed.data.slug;
+      // Grab the duplication check result
       const checkResult = checkDuplication(
         slugList,
         slug,
@@ -95,6 +113,7 @@ const testDuplication = (
     }
   });
 
+  // Set result status to Invalid if there's any error
   if (result.errors.length > 0) {
     result.status = STATUS.INVALID;
   }
