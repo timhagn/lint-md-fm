5d7089f25bc1598124ebfeb7e19cc0ce39db5ece
Tim Hagn <mail@timhagn.com>
2021-09-16 13:56:46 +0200

    adapt test for invalid data

--- src/__tests__/testFrontmatter.test.js
+++ src/__tests__/testFrontmatter.test.js
@@ -1,8 +1,8 @@
 const { testFrontmatter } = require("../testFrontmatter");
 const { DEFAULT_MARKDOWN_EXTENSIONS } = require("../constants");
 
-test("check Frontmatter from file", () => {
-  const changedFiles = ["testFiles/solanamonkeybusiness.md"];
+test("check Frontmatter from file for invalid data", () => {
+  const changedFiles = ["testFiles/invalid_data.md"];
   const directory = "testFiles";
   const frontmatterErrors = testFrontmatter(
     DEFAULT_MARKDOWN_EXTENSIONS,
@@ -14,7 +14,7 @@ Object {
   "errors": Array [
     Object {
       "error": "DATA_INVALID",
-      "file": "testFiles/solanamonkeybusiness.md",
+      "file": "testFiles/invalid_data.md",
     },
   ],
   "status": "INVALID",
