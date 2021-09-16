93436f6046394a9fac9e60151b1855bdb9da2997
Tim Hagn <mail@timhagn.com>
2021-09-16 18:40:37 +0200

    adapt output

--- dist/index.js
+++ dist/index.js
@@ -26564,31 +26564,6 @@ const {
   COMBINED_LOGO_ERRORS,
 } = __nccwpck_require__(4906);
 
-// Error codes and strings for each possible error.
-// const ERRORS = {
-//#   NO_FILES_CHANGED: "NO_FILES_CHANGED", // No relevant files have been changed in the commit.
-//#   EXTENSION_INVALID: "EXTENSION_IS_INVALID", // Markdown or logo extension is not valid.
-//#   DATA_INVALID: "DATA_INVALID", // Markdown is not in valid format.
-
-//#   CATEGORY_INVALID: "CATEGORY_INVALID", // Project category is invalid.
-//#   LOGO_INVALID: "INVALID_LOGO_NAME", // logo value is invalid. e.g. contains white space.
-
-//#   CATEGORY: "CATEGORY_NOT_EXIST", // "category" tag does not exist in the markdown.
-//#   SLUG: "SLUG_NOT_EXIST", // "slug" tag does not exist in the markdown.
-//#   DATE: "DATE_NOT_EXIST", // "date" tag does not exist in the markdown.
-//#   TITLE: "TITLE_NOT_EXIST", // "title" tag does not exist in the markdown.
-//#   LOGLINE: "LOGLINE_NOT_EXIST", // "logline" tag does not exist in the markdown.
-//#   CTA: "CTA_NOT_EXIST", // "cta" tag does not exist in the markdown.
-//#   LOGO: "LOGO_NOT_EXIST", // "logo" tag does not exist in the markdown.
-//#   STATUS: "STATUS_NOT_EXIST", // "state" tag does not exist in the markdown.
-
-//   LOGO_FILE: "LOGO_FILE_NOT_EXIST", // logo file does not exist in the img directory.
-//   LOGO_FORMAT: "INVALID_LOGO_FORMAT", // logo file format does not match the extension.
-//   LOGO_SIZE: "INVALID_LOGO_SIZE", // logo image size is not in correct ratio.
-
-//#   PROJECT_DUPLICATION: "PROJECT_ALREADY_EXIST", // Projects with the same slug exist already.
-// };
-
 /**
  * Get the first error message from results to switch.
  *
@@ -26633,7 +26608,7 @@ const getInvalidDuplicateFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -26666,7 +26641,7 @@ const getMissingTagFilesString = (results) => {
             file
           ].join(", ")}**`
       )
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -26687,7 +26662,7 @@ const getInvalidLogoFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -26710,7 +26685,7 @@ const getInvalidCategoriesFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -26752,7 +26727,7 @@ const getLogoErrorsFilesString = (results) => {
         },
         []
       )
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
--- src/ReporterMessageCreator.js
+++ src/ReporterMessageCreator.js
@@ -5,31 +5,6 @@ const {
   COMBINED_LOGO_ERRORS,
 } = require("./constants");
 
-// Error codes and strings for each possible error.
-// const ERRORS = {
-//#   NO_FILES_CHANGED: "NO_FILES_CHANGED", // No relevant files have been changed in the commit.
-//#   EXTENSION_INVALID: "EXTENSION_IS_INVALID", // Markdown or logo extension is not valid.
-//#   DATA_INVALID: "DATA_INVALID", // Markdown is not in valid format.
-
-//#   CATEGORY_INVALID: "CATEGORY_INVALID", // Project category is invalid.
-//#   LOGO_INVALID: "INVALID_LOGO_NAME", // logo value is invalid. e.g. contains white space.
-
-//#   CATEGORY: "CATEGORY_NOT_EXIST", // "category" tag does not exist in the markdown.
-//#   SLUG: "SLUG_NOT_EXIST", // "slug" tag does not exist in the markdown.
-//#   DATE: "DATE_NOT_EXIST", // "date" tag does not exist in the markdown.
-//#   TITLE: "TITLE_NOT_EXIST", // "title" tag does not exist in the markdown.
-//#   LOGLINE: "LOGLINE_NOT_EXIST", // "logline" tag does not exist in the markdown.
-//#   CTA: "CTA_NOT_EXIST", // "cta" tag does not exist in the markdown.
-//#   LOGO: "LOGO_NOT_EXIST", // "logo" tag does not exist in the markdown.
-//#   STATUS: "STATUS_NOT_EXIST", // "state" tag does not exist in the markdown.
-
-//   LOGO_FILE: "LOGO_FILE_NOT_EXIST", // logo file does not exist in the img directory.
-//   LOGO_FORMAT: "INVALID_LOGO_FORMAT", // logo file format does not match the extension.
-//   LOGO_SIZE: "INVALID_LOGO_SIZE", // logo image size is not in correct ratio.
-
-//#   PROJECT_DUPLICATION: "PROJECT_ALREADY_EXIST", // Projects with the same slug exist already.
-// };
-
 /**
  * Get the first error message from results to switch.
  *
@@ -74,7 +49,7 @@ const getInvalidDuplicateFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -107,7 +82,7 @@ const getMissingTagFilesString = (results) => {
             file
           ].join(", ")}**`
       )
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -128,7 +103,7 @@ const getInvalidLogoFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -151,7 +126,7 @@ const getInvalidCategoriesFilesString = (results) => {
         }
         return accumulatedErrors;
       }, [])
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
@@ -193,7 +168,7 @@ const getLogoErrorsFilesString = (results) => {
         },
         []
       )
-      .join("   ");
+      .join("\n");
   }
   return "";
 };
